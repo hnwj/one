fb2d1d94282b67002464f70534d1dc3e9b9b72e2
hnwj <2948662591@qq.com>
2022-08-30 20:18:29 +0800

    完成了tabbar开发

---
+++ unpackage/dist/dev/mp-weixin/pages/my/my.js
@@ -0,0 +1,33 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/my/my"],{
+
+/***/ 24:
+/*!******************************************************************!*\
+  !*** I:/HTML/12306/firstDemo/main.js?{"page":"pages%2Fmy%2Fmy"} ***!
+  \******************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 4));
+var _my = _interopRequireDefault(__webpack_require__(/*! ./pages/my/my.vue */ 25));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} // @ts-ignore
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;createPage(_my.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 25:
+/*!***********************************************!*\
+  !*** I:/HTML/12306/firstDemo/pages/my/my.vue ***!
+  \***********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'I:\\HTML\\12306\\firstDemo\\pages\\my\\my.vue'");
+
+/***/ })
+
+},[[24,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/my/my.js.map
